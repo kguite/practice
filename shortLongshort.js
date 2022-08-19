b720bb70f43b3204ebb9503a34a5c830cd4d58c7
kguite <kguite@gmail.com>
2022-08-18 19:32:52 -0700

    add shortLongshort.js

---
+++ shortLongshort.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/50654ddff44f800200000007
+
+function solution(a, b){
+    if (a.length > b.length){
+      return b + a + b 
+      } else if (b.length > a.length){
+        return a + b + a
+      }
+    }
+
+    
+    function solution(a, b){
+        return a.length > b.length? b + a + b : a + b + a
+      }
+      
