7aa76f06af0978942ac7d01e4044abbce5f70c14
kguite <kguite@gmail.com>
2022-08-28 16:05:23 -0700

    add cupsOfSugar.js

---
+++ cupsOfSugar.js
@@ -0,0 +1,10 @@
+// Codecademy do... while practice 
+
+let cupsOfSugarNeeded = 6
+let cupsAdded = 0
+let i = 0
+
+do {
+  cupsAdded++;
+console.log(cupsAdded)
+} while (cupsAdded < cupsOfSugarNeeded);
