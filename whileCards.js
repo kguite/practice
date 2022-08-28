7d0c10749f083c5b900deb28717130978f13f3ed
kguite <kguite@gmail.com>
2022-08-28 16:04:43 -0700

    add whileCards.js

---
+++ whileCards.js
@@ -0,0 +1,12 @@
+// codecademy while loop practice
+
+const cards = ['diamond', 'spade', 'heart', 'club'];
+
+// Write your code below
+let currentCard 
+while (currentCard != 'spade') {
+currentCard = cards[Math.floor(Math.random() * 4)];
+console.log(currentCard)
+}
+
+
